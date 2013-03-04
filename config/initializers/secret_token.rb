2d59bdb41f1119d5188e552679cddd2d316800bf
doctordray <acheam@gmail.com>
2013-03-04 15:37:22 +1100

    ac:initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iveta::Application.config.secret_token = 'd366adc088bb688cde15e57aa66e757d937358a9e69fbb02c7691dd6d4ff28891be96d5fcaed6cff6cb1792224d1d075b532094bb3930c5256337824d26e899c'
