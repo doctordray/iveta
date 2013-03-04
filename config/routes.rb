f313333477a2266eed4569d565084be522c1c2d6
doctordray <acheam@gmail.com>
2013-03-04 20:38:38 +1100

    ac: created static movies page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Iveta::Application.routes.draw do
+  get "movies/home"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +50,7 @@ Iveta::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'movies#home'
 
   # See how all your routes lay out with "rake routes"
 
